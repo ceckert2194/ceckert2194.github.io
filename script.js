775e43693036ed6ab87692a64d52451f63703a93
ceckert2194 <ceckert2194@gmail.com>
2021-10-03 13:26:54 -0400

    added hamburger menu, thanks to devggaurav@dev.to for the tutorial on them

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ var nav = document.getElementById('navbar');
 //         nav.style.visibility = 'hidden';
 //     }
 // }
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.list-links');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
